d87887f5e5e8efaf5c014ddd338244c7dba5021b
Shaurya Panchal <122795698+shauryapanchal@users.noreply.github.com>
2025-09-17 12:26:18 +0000

    Testing commit 2

---
+++ src/components/Row.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Tile from "./Tile";
+
+function Row(){
+    return (
+        <></>
+    );
+}
+
+export default Row;
---
+++ src/components/Tile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { RxBox } from "react-icons/rx";
+
+function Tile(){
+    return (
+        <RxBox size={85}/>
+    );
+}
+
+export default Tile;
