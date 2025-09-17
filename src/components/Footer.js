52f1b45ad1a2afdb6fc9bb55ecff4ad2107b21af
Shaurya Panchal <122795698+shauryapanchal@users.noreply.github.com>
2025-09-17 11:05:04 +0000

    Removed default code and introduced Footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css"
+function Footer() {
+  return (
+    <footer>
+        Made By Shaurya and Gurashish
+    </footer>
+  );
+}
+
+export default Footer;
